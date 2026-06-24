1f7ef46f7d3402118fa16be3c9e6e7d626b928d3
wlsdn020416 <wlsdn020416@gmail.com>
2026-06-24 17:35:59 +0900

    feat: TradeService 생성

--- backend/src/main/java/com/stocksim/dto/TradeResponse.java
+++ backend/src/main/java/com/stocksim/dto/TradeResponse.java
@@ -1,4 +1,30 @@
 package com.stocksim.dto;
 
-public class TradeResponse {
+import com.stocksim.entity.Trade;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record TradeResponse ( // 프론트로 보낼 데이터 목록
+    Long id,
+    Long userId,
+    Long stockId,
+    String tradeType,
+    Integer quantity,
+    BigDecimal price,
+    BigDecimal totalAmount,
+    LocalDateTime createdAt
+) {
+    public static TradeResponse from(Trade trade) { // Entity 하나를 Response 하나로 바꿈
+        return new TradeResponse(
+                trade.getId(),
+                trade.getUserId(),
+                trade.getStockId(),
+                trade.getTradeType(),
+                trade.getQuantity(),
+                trade.getPrice(),
+                trade.getTotalAmount(),
+                trade.getCreatedAt()
+        );
+    }
 }
--- backend/src/main/java/com/stocksim/service/TradeService.java
+++ backend/src/main/java/com/stocksim/service/TradeService.java
@@ -1,4 +1,25 @@
 package com.stocksim.service;
 
+import com.stocksim.dto.TradeResponse;
+import com.stocksim.repository.TradeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service // 비즈니스 로직 담당
 public class TradeService {
+
+    private  final TradeRepository tradeRepository; // 입력받은 레포지토리는 바뀌지 않는다
+    public TradeService(TradeRepository tradeRepository) {
+        this.tradeRepository = tradeRepository;
+    }
+    //현재 로그인한 사용자 거래내역 조회
+    public List<TradeResponse> findMyTrades(){
+        Long userId = 1L; // 유저 아이디가 없으므로 1로 고정
+
+        return tradeRepository.findByUserId(userId)
+                .stream() // 내부의 값을 하나씩 꺼낸다
+                .map(TradeResponse::from) // DTO 변환
+                .toList(); // 리스트로 변환
+    }
 }
