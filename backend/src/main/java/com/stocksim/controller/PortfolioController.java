a1b4f3974eba6ba8864c2f320af80980fbf80be6
sangmin <tjfl9831@gmail.com>
2026-06-29 17:48:06 +0900

    feat: 포트폴리오 기능 추가

---
+++ backend/src/main/java/com/stocksim/controller/PortfolioController.java
@@ -0,0 +1,19 @@
+package com.stocksim.controller;
+
+import com.stocksim.service.PortfolioService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PortfolioController {
+    private final PortfolioService portfolioService;
+    public PortfolioController(PortfolioService portfolioService) {
+        this.portfolioService = portfolioService;
+    }
+
+    @GetMapping("api/portfolio/me") // 포트폴리오 가져오기
+    public void getPortfolio() { // TODO : 리턴 타입 변경 + service 로직 변경
+        portfolioService.getPortfolio();
+    }
+
+}
---
+++ backend/src/main/java/com/stocksim/dto/PortfolioResponse.java
@@ -0,0 +1,5 @@
+package com.stocksim.dto;
+
+public record PortfolioResponse() { // TODO : JPA 공부해서 채워 넣기
+
+}
