1427e0379322ac66940828afe5c0715bbd614f64
sangmin <tjfl9831@gmail.com>
2026-07-01 17:39:02 +0900

    docs: 포트폴리오 서비스 주석 추가

--- backend/src/main/java/com/stocksim/controller/PortfolioController.java
+++ backend/src/main/java/com/stocksim/controller/PortfolioController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/portfolio")
--- backend/src/main/java/com/stocksim/service/PortfolioService.java
+++ backend/src/main/java/com/stocksim/service/PortfolioService.java
@@ -35,9 +35,9 @@ public class PortfolioService {
         Long userId = user.getId();
 
         List<Portfolio> portfolios =
-                portfolioRepository.findByUserId(userId);
+                portfolioRepository.findByUserId(userId); // 특정유저의 특정주식 포트폴리오 정보 전부 가져와서 리스트에 담기
 
-        List<PortfolioStockResponse> stocks = new ArrayList<>();
+        List<PortfolioStockResponse> stocks = new ArrayList<>(); // 자식DTO 정보 담을 리스트 생성
 
         BigDecimal totalStockAsset = BigDecimal.ZERO;
         BigDecimal totalPurchase = BigDecimal.ZERO;
@@ -45,15 +45,15 @@ public class PortfolioService {
         for (Portfolio portfolio : portfolios) {
 
             Stock stock = stockRepository.findById(portfolio.getStockId())
-                    .orElseThrow();
+                    .orElseThrow(); // 포트폴리오의 주식id로 주식 정보 가져오기
 
             BigDecimal currentValue =
                     stock.getCurrentPrice()
-                            .multiply(BigDecimal.valueOf(portfolio.getQuantity()));
+                            .multiply(BigDecimal.valueOf(portfolio.getQuantity())); // 현재가 × 보유 수량
 
             BigDecimal purchaseValue =
                     portfolio.getAverageBuyPrice()
-                            .multiply(BigDecimal.valueOf(portfolio.getQuantity()));
+                            .multiply(BigDecimal.valueOf(portfolio.getQuantity())); // 평균 매수가 × 보유 수량
 
             stocks.add(
                     new PortfolioStockResponse(
@@ -64,17 +64,17 @@ public class PortfolioService {
                             stock.getCurrentPrice(),
                             currentValue
                     )
-            );
+            ); // 자식 DTO 리스트에 자식 DTO add
 
             totalStockAsset = totalStockAsset.add(currentValue);
             totalPurchase = totalPurchase.add(purchaseValue);
         }
 
         BigDecimal totalAsset =
-                user.getCash().add(totalStockAsset);
+                user.getCash().add(totalStockAsset); // 현금 + 보유 주식 평가금액
 
         BigDecimal totalProfit =
-                totalAsset.subtract(totalPurchase);
+                totalAsset.subtract(totalPurchase); // 현재 자산 - 내가 투자한 금액
 
         return new PortfolioResponse(
                 user.getId(),
