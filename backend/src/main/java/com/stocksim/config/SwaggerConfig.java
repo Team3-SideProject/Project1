3b0dc13ef17042541bab6016003597cac3421f15
wlsdn020416 <wlsdn020416@gmail.com>
2026-07-07 16:26:34 +0900

    merge 이전 커밋

--- backend/src/main/java/com/stocksim/config/SwaggerConfig.java
+++ backend/src/main/java/com/stocksim/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.stocksim.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +12,6 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         String securityJwtName = "JWT_AUTH";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityJwtName);
-
         // 🌟 핵심 변경: 스웨거가 자동으로 Bearer를 붙이지 않고, 사용자가 입력한 그대로 'Header'에 꽂아주도록 설정
         Components components = new Components().addSecuritySchemes(securityJwtName,
                 new SecurityScheme()
@@ -23,7 +20,6 @@ public class SwaggerConfig {
                         .in(SecurityScheme.In.HEADER));    // 헤더에 담아 전송
 
         return new OpenAPI()
-                .addSecurityItem(securityRequirement)
                 .components(components);
     }
 }
